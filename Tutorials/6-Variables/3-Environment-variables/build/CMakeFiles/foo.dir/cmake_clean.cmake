2fe01ff2fe781ee6f1c4371f7710921f9d243d64
Stevedan Ogochukwu Omodolor <stevedanogochukwu.omodolor@student.kuleuven.be>
2020-07-18 21:16:44 +0200

    environment variables

--- Tutorials/6-Variables/3-Environment-variables/build/CMakeFiles/foo.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/foo"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/foo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
